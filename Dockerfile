85774f9e71cf0dcd814ff99581cb05cd8e458cb7
jazielloureiro <jazielloureiro17@gmail.com>
2024-02-19 08:29:05 -0300

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.22 AS build
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+
+RUN go mod download
+
+COPY main.go .
+
+RUN CGO_ENABLED=0 go build -o server
+
+FROM scratch
+
+COPY --from=build /app/server /
+
+CMD ["/server"]
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handleStatements(rw http.ResponseWriter, req *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+	fmt.Fprintf(rw, "Hello, %v!\n", req.PathValue("id"))
+}
+
+func main() {
+	http.HandleFunc("GET /clientes/{id}/extrato", handleStatements)
+
+	addr := fmt.Sprintf(":%v", os.Getenv("PORT"))
+
+	http.ListenAndServe(addr, nil)
+}
