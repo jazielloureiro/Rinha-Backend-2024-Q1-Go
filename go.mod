1d74db396ae8e889baa6bf5cc6ccb45ffddd83a5
jazielloureiro <jazielloureiro17@gmail.com>
2024-02-19 11:04:23 -0300

    Add basic postgres driver setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jazielloureiro/Rinha-Backend-2024-Q1-Go
 
 go 1.22
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
+type Account struct {
+	Id    int
+	Limit int
+	Value int
+}
+
 func handleStatements(rw http.ResponseWriter, req *http.Request) {
+	connectionString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"))
+
+	db, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	row := db.QueryRow("SELECT * FROM \"Account\" WHERE \"Id\" = $1", req.PathValue("id"))
+
+	err = row.Err()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var account Account
+	row.Scan(&account.Id, &account.Limit, &account.Value)
+
 	rw.WriteHeader(http.StatusOK)
-	fmt.Fprintf(rw, "Hello, %v!\n", req.PathValue("id"))
+	fmt.Fprintf(rw, "Hello, %v!\n", account)
 }
 
 func main() {
