d9a3329eea76689224b0a5ca88db71676aa979fc
jazielloureiro <jazielloureiro17@gmail.com>
2024-02-21 21:24:02 -0300

    Add AccountId foreign key in Statement table

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -7,16 +7,9 @@ CREATE TABLE "Account" (
 
 CREATE TABLE "Statement" (
     "Id" SERIAL PRIMARY KEY,
+    "AccountId" INTEGER REFERENCES "Account" ("Id"),
     "Value" INTEGER NOT NULL,
     "Type" CHAR NOT NULL,
     "Description" VARCHAR(10) NOT NULL,
     "Date" TIMESTAMP NOT NULL
 );
-
-INSERT INTO "Account" ("Id", "Limit", "Value")
-VALUES
-    (1, 100000, 0),
-    (2, 80000, 0),
-    (3, 1000000, 0),
-    (4, 10000000, 0),
-    (5, 500000, 0);
